5666363bdfc56a2cea50cfe6db3e118f32eb699e
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2024-01-12 22:02:45 +0900

    ヘッダーとフッターの作成

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=yes, maximum-scale=1.0, minimum-scale=1.0">
+<title>ウルトラマン フュージョンファイト！ ウルトラファイル 専用サイト</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<script src="js/css3-mediaqueries.js"></script>
+<![endif]-->
+<script src="js/jquery1.7.2.min.js"></script>
+<script src="js/script.js"></script>
+</head>
+
+<body>
+
+<!-- ヘッダー読み込みー -->
+	<?php include "header.html" ?>
+
+	<div id="wrapper">
+  
+  <!-- コンテンツ -->
+	<section id="main">
+  
+  	<!-- メイン画像 -->
+		<div id="mainBanner">
+			<img src="images/mainImg2.jpg" width="680" height="140" alt="">
+    	<div class="slogan">
+				<h2>自然との調和を目指す企業です</h2>
+				<p>ホームページサンプル株式会社では最新技術と自然との調和を目指します。<br>革新的な技術で世の中を動かす企業を目指します。</p>
+			</div>
+		</div>
+		<!-- / メイン画像 -->
+    
+    <section class="content">
+    	<h3 class="heading">ホームページサンプル株式会社の取り組み</h3>
+      <article>
+			<img src="images/sample1.jpg" width="210" height="120" alt="" class="alignright border" />
+			<p>ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な技術で世の中を動かす企業を目指します。ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な技術で世の中を動かす企業を目指します。ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な技術で世の中を動かす。革新的な技術で世の中を動かす企業を目指します。ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な技術で世の中を動かす。革新的な技術で世の中を動かす。</p>
+    	</article>
+    </section>
+    
+    <section class="content">
+			<h3 class="heading">ホームページサンプル株式会社の取り組み</h3>
+      <article>
+			  <img src="images/sample2.jpg" width="210" height="120" alt="" class="alignleft border" />
+				<p>革新的な技術で世の中を動かす企業を目指します。ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な技術で世の中を動かす企業を目指します。</p>
+				<p>ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な技術で世の中を動かす企業を目指します。ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な技術で世の中を動かす。革新的な技術で世の中を動かす。</p>
+      </article>
+		</section>
+    
+		<section class="content">
+			<h3 class="heading">会社概要</h3>
+      <article>
+				<table class="table">
+				<tr>
+					<th>会社名</th>
+					<td>ホームページサンプル株式会社（英語表記 Homepage sample Inc.）</td>
+				</tr>
+				<tr>
+					<th>設立</th>
+					<td>平成10年1月10日</td>
+				</tr>
+				<tr>
+					<th>事業内容</th>
+					<td>IT・マーケティング・福祉など</td>
+				</tr>
+				<tr>
+					<th>住所</th>
+					<td>〒012-3456 見本県見本市サンプル1-2</td>
+				</tr>
+				<tr>
+					<th>電話番号</th>
+					<td>0123-4567-89012</td>
+				</tr>
+				<tr>
+					<th>メールアドレス</th>
+					<td>info@example.com</td>
+				</tr>
+				</table>
+    	</article>
+		</section>
+    
+    <section class="content" id="gallery">
+			<h3 class="heading">写真ギャラリー</h3>
+				<article>
+					<figure class="grid"><a href="subpage.html"><img src="images/gallery1.jpg" width="210" height="120" alt=""></a></figure>
+					<figure class="grid"><a href="subpage.html"><img src="images/gallery2.jpg" width="210" height="120" alt=""></a></figure>
+					<figure class="grid"><a href="subpage.html"><img src="images/gallery3.jpg" width="210" height="120" alt=""></a></figure>
+					<figure class="grid"><a href="subpage.html"><img src="images/gallery4.jpg" width="210" height="120" alt=""></a></figure>
+					<figure class="grid"><a href="subpage.html"><img src="images/gallery5.jpg" width="210" height="120" alt=""></a></figure>
+					<figure class="grid"><a href="subpage.html"><img src="images/gallery6.jpg" width="210" height="120" alt=""></a></figure>
+				</article>
+		</section>
+    
+	</section>
+	<!-- / コンテンツ -->
+
+	<aside id="sidebar">
+       
+		<h3 class="heading">革新的な技術</h3>
+    <article>
+			<ul>
+				<li><a href="subpage.html">環境への取り組みについての説明ページです</a></li>
+				<li><a href="subpage.html">ecoキャンペーン開催中です</a></li>
+				<li><a href="subpage.html">オフィスの移転に関して</a></li>
+				<li><a href="subpage.html">最新商品のご紹介</a></li>
+				<li><a href="subpage.html">新規サービスを開始しました</a></li>
+				<li><a href="subpage.html">環境賞受賞に関してはこちらをご確認ください</a></li>
+			</ul>
+    </article>
+    
+	</aside>
+ 
+</div>
+ 
+<!-- フッター読み込み -->
+	<?php include "footer.html" ?>
+
+</body>
+</html>
