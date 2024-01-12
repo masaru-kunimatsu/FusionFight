afa4fbe2563717a31c8045e5aae4c5d6136776b8
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2024-01-12 23:29:45 +0900

    index

---
+++ db_connect.php
@@ -0,0 +1,4 @@
+#データベースに接続
+$dsn = 'mysql:host=localhost; dbname=fusionfight; charset=utf8';
+$user = 'testuser';
+$pass = 'testpass';
---
+++ index_tmpl.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=yes, maximum-scale=1.0, minimum-scale=1.0">
+  <title>ウルトラマン フュージョンファイト！ ウルトラファイル 専用サイト</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+  <!--[if lt IE 9]>
+  <script src="js/html5.js"></script>
+  <script src="js/css3-mediaqueries.js"></script>
+  <![endif]-->
+  <script src="js/jquery1.7.2.min.js"></script>
+  <script src="js/script.js"></script>
+</head>
+
+<body>
+
+<header id="header">
+  <!-- ロゴ -->
+	<div class="logo">
+		<a href="index.php"><img src="bnr_ultrafile.png" width="20%"><span>専用サイト</span></a>
+	</div>
+	<!-- / ロゴ -->
+	<!-- メインナビゲーション -->
+	<nav id="mainNav">
+		<div class="inner">
+			<a class="menu" id="menu"><span>MENU</span></a>
+			<div class="panel">   
+				<ul>
+					<li class="active"><a href="index.php"><strong>カード検索</strong></a></li>
+					<li><a href="subpage.html"><strong>デッキ作成</strong></a></li>
+					<li><a href="subpage.html"><strong>デッキ確認</strong></a></li>
+					<li class="last"><a href="subpage.html"><strong>ログイン</strong></a></li>
+				</ul>   
+			</div>
+		</div> 
+	</nav>
+	<!-- / メインナビゲーション -->
+</header>
+	<div id="wrapper">
+  
+  <!-- コンテンツ -->
+	<section id="main">
+		<h1>CHECK!!</h1>
+		★検索結果★
+	</section>
+	<!-- / コンテンツ -->
+
+	<aside id="sidebar">
+       
+		<h3 class="heading">革新的な技術</h3>
+    <article>
+			<ul>
+				<li><a href="subpage.html">環境への取り組みについての説明ページです</a></li>
+				<li><a href="subpage.html">ecoキャンペーン開催中です</a></li>
+				<li><a href="subpage.html">オフィスの移転に関して</a></li>
+				<li><a href="subpage.html">最新商品のご紹介</a></li>
+				<li><a href="subpage.html">新規サービスを開始しました</a></li>
+				<li><a href="subpage.html">環境賞受賞に関してはこちらをご確認ください</a></li>
+			</ul>
+    </article>
+
+    
+	</aside>
+ 
+</div>
+ 
+<footer id="footer">
+	<div class="inner">
+  	<a href="https://dcd-ultraman.com/"><img src="img_ogp.jpg" width="20%"></a>
+		<a href="https://play.google.com/store/apps/details?id=com.bandai.ultramanbinder&hl=en_US"><img src="img_apri.png" width="20%"></a>
+		<a href="https://imagination.m-78.jp/"><img src="img_ti.jpg" width="20%"></a>
+	</div>
+</footer>
+
+</body>
+</html>
